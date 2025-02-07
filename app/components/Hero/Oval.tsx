16577e783066b21e5284e76a26bb92bb61aa5b1b
Daniel Martins <martinsda@gmail.com>
2025-02-07 01:53:35 -0300

    edit styling

--- app/components/Hero/Oval.tsx
+++ app/components/Hero/Oval.tsx
@@ -1,6 +1,6 @@
 const Oval = () => {
   return (
-    <div className="hidden sm:flex absolute w-[1022px] h-[1022px] -left-[554px] -top-[493px] bg-[linear-gradient(147.31deg,_rgba(141,_141,_236,_0.167177)_45.74%,_rgba(84,_84,_212,_0.0001)_86.05%)] rounded-full" />
+    <div className="flex absolute w-[1022px] h-[1022px] -left-[554px] -top-[493px] bg-[linear-gradient(147.31deg,_rgba(141,_141,_236,_0.167177)_45.74%,_rgba(84,_84,_212,_0.0001)_86.05%)] rounded-full" />
   );
 };
 
--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -8,31 +8,29 @@ import Button from "../ui/Button";
 
 const Hero = () => {
   return (
-    <main>
-      <CenteredElement className="h-[90vh]">
-        <Container className="flex flex-row gap-x-4">
-          <CenteredElement
-            className="h-full gap-y-10"
-            direction="col"
-            items="start"
-          >
-            <Typography className="text-6xl text-[#008CFF]" weight="700">
-              Invista em arte e patrocine sonhos!
-            </Typography>
-            <Typography className="text-lg">
-              A primeira criptomoeda de apoio e doações para jovens talentos da
-              dança.
-            </Typography>
-            <Button label="Comece Agora" />
-          </CenteredElement>
-          <CenteredElement className="hidden md:flex relative w-2/3 h-full">
-            <Image className="scale-90 z-20" src={Ballerina} alt="ballerina" />
-            <div className="absolute top-[27%] right-[15%] w-[65%] aspect-square bg-yellow-500/10 rounded-full z-0" />
-          </CenteredElement>
-        </Container>
-        <Oval />
-      </CenteredElement>
-    </main>
+    <CenteredElement className="h-[90vh]">
+      <Container className="flex flex-row gap-x-4">
+        <CenteredElement
+          className="h-full gap-y-10"
+          direction="col"
+          items="start"
+        >
+          <Typography className="text-6xl text-[#008CFF]" weight="700">
+            Invista em arte e patrocine sonhos!
+          </Typography>
+          <Typography className="text-lg">
+            A primeira criptomoeda de apoio e doações para jovens talentos da
+            dança.
+          </Typography>
+          <Button label="Comece Agora" />
+        </CenteredElement>
+        <CenteredElement className="hidden md:flex relative w-2/3 h-full">
+          <Image className="scale-90 z-20" src={Ballerina} alt="ballerina" />
+          <div className="absolute top-[27%] right-[15%] w-[65%] aspect-square bg-yellow-500/10 rounded-full z-0" />
+        </CenteredElement>
+      </Container>
+      <Oval />
+    </CenteredElement>
   );
 };
 
