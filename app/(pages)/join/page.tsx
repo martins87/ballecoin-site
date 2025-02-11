1cf06c150370e26380b158822feac023a9bfbdc5
Daniel Martins <martinsda@gmail.com>
2025-02-11 09:51:17 -0300

    edit about and join pages

--- app/(pages)/join/page.tsx
+++ app/(pages)/join/page.tsx
@@ -1,5 +1,22 @@
+import Typography from "@/app/components/Typography";
+import Container from "@/app/components/ui/Container";
+
 const JoinPage = () => {
-  return <div>JoinPage</div>;
+  return (
+    <Container className="my-20">
+      <div className="w-[90%] sm:w-[75%] mb-20 text-center">
+        <Typography className="text-4xl sm:text-6xl text-black" weight="700">
+          How to
+        </Typography>
+        <Typography
+          className="text-4xl sm:text-6xl text-[#008CFF]"
+          weight="700"
+        >
+          &nbsp;Join
+        </Typography>
+      </div>
+    </Container>
+  );
 };
 
 export default JoinPage;
