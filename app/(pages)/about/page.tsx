454c6dcde255cdf14918e6b05da16a8d22c7169f
Daniel Martins <martinsda@gmail.com>
2025-02-13 10:50:47 -0300

    add token address

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import Link from "next/link";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 import Typography from "@/app/components/Typography";
 import Container from "@/app/components/ui/Container";
 import AboutToken from "../../components/About";
+import CenteredElement from "@/app/components/ui/CenteredElement";
+import ExternalLink from "../../assets/icons/external-link.svg";
 
 const AboutPage = () => {
   return (
@@ -25,6 +29,24 @@ const AboutPage = () => {
           &nbsp;BalleCoin
         </Typography>
       </motion.div>
+      <CenteredElement className="gap-y-2" direction="col">
+        <Typography weight="600">Token address:</Typography>
+        <Link
+          className="flex items-center justify-between gap-x-3"
+          href="https://solscan.io/token/7fpj12mqovZuDPGB3VQ1nCYkG5HVdLN6g9ZjCUwCGxu9"
+          target="_blank"
+          rel="noopener"
+        >
+          <Typography className="text-xs md:text-2xl">
+            7fpj12mqovZuDPGB3VQ1nCYkG5HVdLN6g9ZjCUwCGxu9
+          </Typography>
+          <Image
+            className="w-3 md:w-5"
+            src={ExternalLink}
+            alt="external link"
+          />
+        </Link>
+      </CenteredElement>
       <AboutToken />
     </Container>
   );
