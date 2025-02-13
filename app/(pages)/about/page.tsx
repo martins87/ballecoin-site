24bdad994720ec18ab557831a21ac9cd47a54297
Daniel Martins <martinsda@gmail.com>
2025-02-13 11:05:06 -0300

    add decentralized exchanges

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -9,6 +9,8 @@ import Container from "@/app/components/ui/Container";
 import AboutToken from "../../components/About";
 import CenteredElement from "@/app/components/ui/CenteredElement";
 import ExternalLink from "../../assets/icons/external-link.svg";
+import Raydium from "../../assets/icons/raydium.png";
+import Jupiter from "../../assets/icons/jupiter.png";
 
 const AboutPage = () => {
   return (
@@ -29,15 +31,17 @@ const AboutPage = () => {
           &nbsp;BalleCoin
         </Typography>
       </motion.div>
-      <CenteredElement className="gap-y-2" direction="col">
-        <Typography weight="600">Token address:</Typography>
+      <CenteredElement className="gap-y-2 md:gap-y-3" direction="col">
+        <Typography className="text-xl" weight="600">
+          Token address:
+        </Typography>
         <Link
           className="flex items-center justify-between gap-x-3"
           href="https://solscan.io/token/7fpj12mqovZuDPGB3VQ1nCYkG5HVdLN6g9ZjCUwCGxu9"
           target="_blank"
           rel="noopener"
         >
-          <Typography className="text-xs md:text-2xl">
+          <Typography className="text-xs md:text-xl">
             7fpj12mqovZuDPGB3VQ1nCYkG5HVdLN6g9ZjCUwCGxu9
           </Typography>
           <Image
@@ -46,6 +50,35 @@ const AboutPage = () => {
             alt="external link"
           />
         </Link>
+      </CenteredElement>
+      <CenteredElement className="gap-y-2 md:gap-y-3 mt-10" direction="col">
+        <Typography className="text-xl" weight="600">
+          Buy on:
+        </Typography>
+        <CenteredElement className="gap-x-10">
+          <Link
+            className="flex items-center justify-between gap-x-2"
+            href="https://raydium.io/swap/?inputMint=sol&outputMint=7fpj12mqovZuDPGB3VQ1nCYkG5HVdLN6g9ZjCUwCGxu9"
+            target="_blank"
+            rel="noopener"
+          >
+            <Image className="w-6" src={Raydium} alt="external link" />
+            <Typography className="text-lg md:text-xl" font="inter">
+              Raydium
+            </Typography>
+          </Link>
+          <Link
+            className="flex items-center justify-between gap-x-2"
+            href="https://jup.ag/swap/SOL-7fpj12mqovZuDPGB3VQ1nCYkG5HVdLN6g9ZjCUwCGxu9"
+            target="_blank"
+            rel="noopener"
+          >
+            <Image className="w-6" src={Jupiter} alt="external link" />
+            <Typography className="text-lg md:text-xl" font="inter">
+              Jupiter
+            </Typography>
+          </Link>
+        </CenteredElement>
       </CenteredElement>
       <AboutToken />
     </Container>
