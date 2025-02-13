f8c0dd6b84fcc2940433e36f925b07e81afcbd71
Daniel Martins <martinsda@gmail.com>
2025-02-12 22:33:03 -0300

    fix linting

--- app/api/dancers/[email]/route.ts
+++ app/api/dancers/[email]/route.ts
@@ -3,10 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import connectMongoDB from "../../../lib/db";
 import Dancer from '../../../models/dancer';
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { email: string } }
-){
+export async function GET(request: NextRequest, { params }){
   const { email } = params;
   await connectMongoDB();
   const dancer = await Dancer.findOne({ email });
