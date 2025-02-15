75f1a97f1f6e342bc52dd5236cecf933955e7a52
Cristian Solutchak <cristiansolutchak@gmail.com>
2025-02-15 03:48:19 -0300

    Implement change color and text state

--- app/components/HowSection/index.tsx
+++ app/components/HowSection/index.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { useState } from "react";
 import Typography from "../Typography";
 import CenteredElement from "../ui/CenteredElement";
 import Container from "../ui/Container";
 import HowToJoinCard from "./HowToJoinCard";
 
 const HowSection = () => {
+  const [isSponsor, setIsSponsor] = useState(true); 
+
+  const handleToggle = () => {
+    setIsSponsor(!isSponsor); 
+  };
+
   return (
     <Container className="mt-32 mb-56">
       <div id="how-section" className="w-[90%] sm:w-[75%] mb-14 text-center">
         <Typography className="text-4xl sm:text-6xl text-black" weight="700">
-          {/* Como fazer parte desse */}
           How to be part of this
           <br />
         </Typography>
@@ -18,32 +24,45 @@ const HowSection = () => {
           className="text-4xl sm:text-6xl text-[#008CFF]"
           weight="700"
         >
-          &nbsp;moviment
+          &nbsp;movement
         </Typography>
         <Typography className="text-4xl sm:text-6xl text-black" weight="700">
           ?
         </Typography>
       </div>
       <CenteredElement className="w-4/5 sm:w-[65%] md:w-[50%]">
-        <CenteredElement className="py-2 px-4 sm:px-8 tablet:px-2 bg-[linear-gradient(96.71deg,_#5656F2_0%,_#3333DC_101.64%)] [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] hover:cursor-pointer">
+        <CenteredElement
+          onClick={handleToggle}
+          className={`py-2 px-4 sm:px-8 tablet:px-2 ${
+            isSponsor
+              ? "bg-[linear-gradient(96.71deg,_#5656F2_0%,_#3333DC_101.64%)]"
+              : "bg-gray-300"
+          } [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] hover:cursor-pointer`}
+        >
           <Typography className="text-sm sm:text-base text-white" weight="700">
-            {/* Para Doadores */}
             For Sponsors
           </Typography>
         </CenteredElement>
-        <CenteredElement className="py-2 px-4 sm:px-8 tablet:px-2 bg-[linear-gradient(96.71deg,_rgba(86,_86,_242,_0.4)_0%,_rgba(51,_51,_220,_0.4)_101.64%)] [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] -rotate-180 hover:cursor-pointer">
+        <CenteredElement
+          onClick={handleToggle}
+          className={`py-2 px-4 sm:px-8 tablet:px-2 ${
+            !isSponsor
+              ? "bg-[linear-gradient(96.71deg,_#F2994A_0%,_#F2C94C_101.64%)]"
+              : "bg-gray-300"
+          } [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] -rotate-180 hover:cursor-pointer`}
+        >
           <Typography
             className="text-sm sm:text-base text-white -rotate-180"
             weight="700"
           >
-            {/* Para Artistas */}
             For Artists
           </Typography>
         </CenteredElement>
       </CenteredElement>
-      <HowToJoinCard />
+      <HowToJoinCard isSponsor={isSponsor} />
     </Container>
   );
 };
 
 export default HowSection;
+
