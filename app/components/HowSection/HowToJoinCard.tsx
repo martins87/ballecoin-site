df60e4a8828a46c7e344c7bf1cae9fdcf03d7474
Daniel Martins <martinsda@gmail.com>
2025-02-15 21:20:28 -0300

    edit how to join section

--- app/components/HowSection/HowToJoinCard.tsx
+++ app/components/HowSection/HowToJoinCard.tsx
@@ -1,23 +1,18 @@
-import { redirect } from "next/navigation";
-
 import Typography from "../Typography";
-import Button from "../ui/Button";
 import CenteredElement from "../ui/CenteredElement";
+import { twMerge } from "tailwind-merge";
 
-const steps = [
+const sponsorSteps = [
   {
     index: 1,
-    // label: "Crie sua conta",
-    label: "Create you account",
+    label: "Create your account",
   },
   {
     index: 2,
-    // label: "Compre Ballecoins",
     label: "Buy BalleCoins",
   },
   {
     index: 3,
-    // label: "Doe para um talento ou utilize seus tokens",
     label: "Donate for a talent or utilize your tokens",
   },
   {
@@ -26,40 +21,65 @@ const steps = [
   },
 ];
 
-const HowToJoinCard = () => {
-  const handleClick = () => redirect("/join");
+
+const artistSteps = [
+  {
+    index: 1,
+    label: "Create your artist profile",
+  },
+  {
+    index: 2,
+    label: "Create your solana wallet",
+  },
+  {
+    index: 3,
+    label: "Configure your profile to receive donations or tokens",
+  },
+  {
+    index: 4,
+    label: "Grow your artistic career",
+  },
+];
+
+interface HowToJoinCardProps {
+  isSponsor: boolean; 
+}
+
+
+const HowToJoinCard = ({ isSponsor }: HowToJoinCardProps) => {
+  const steps = isSponsor ? sponsorSteps : artistSteps; 
 
   return (
     <CenteredElement
-      className="w-[90%] sm:w-3/5 sm:[box-shadow:0px_202.825px_490.554px_rgba(120,_120,_168,_0.17)] sm:border sm:shadow-lg rounded-[40px] mt-6 sm:px-8 py-10"
+      className={twMerge(
+        "w-[75%] tablet:w-3/5 sm:[box-shadow:0px_202.825px_490.554px_rgba(120,_120,_168,_0.17)] sm:border sm:shadow-lg rounded-[40px] mt-6 sm:px-8 py-10",
+      )}
       direction="col"
     >
       <CenteredElement
-        className="tablet:w-4/5 border-b border-[#3939DF]/10 pb-8"
+        // className={`tablet:w-4/5`}
         direction="col"
       >
         {steps.map((step) => (
           <CenteredElement
             key={step.index}
-            className="gap-x-10 sm:gap-x-4"
+            className="gap-x-10 sm:gap-x-4 ml-10"
             justify="start"
           >
             <CenteredElement className="w-[5vw]">
               <Typography
-                className="text-6xl text-[#3939DF]/10 text-center"
+                className={`text-6xl text-[#3939DF]/20 text-center`}
                 weight="700"
               >
                 {step.index}
               </Typography>
             </CenteredElement>
-            <Typography className="text-lg tablet:text-xl" weight="500">
+            <Typography className="text-lg tablet:text-xl text-black/80" weight="500">
               {step.label}
             </Typography>
           </CenteredElement>
         ))}
       </CenteredElement>
-      {/* <Button className="mt-8" label="Ver detalhadamente" /> */}
-      <Button className="mt-8" label="See Details" onClick={handleClick} />
     </CenteredElement>
   );
 };
--- app/components/HowSection/index.tsx
+++ app/components/HowSection/index.tsx
@@ -1,16 +1,26 @@
 "use client";
 
+import { useState } from "react";
+import { redirect } from "next/navigation";
+
 import Typography from "../Typography";
 import CenteredElement from "../ui/CenteredElement";
 import Container from "../ui/Container";
 import HowToJoinCard from "./HowToJoinCard";
+import Button from "../ui/Button";
+import { twMerge } from "tailwind-merge";
 
 const HowSection = () => {
+  const [isSponsor, setIsSponsor] = useState(true); 
+
+  const handleClick = () => redirect("/join");
+
+  const handleToggle = () => setIsSponsor(!isSponsor); 
+
   return (
     <Container className="mt-32 mb-56">
-      <div className="w-[90%] sm:w-[75%] mb-14 text-center">
+      <div id="how-section" className="w-[90%] sm:w-[75%] mb-14 text-center">
         <Typography className="text-4xl sm:text-6xl text-black" weight="700">
-          {/* Como fazer parte desse */}
           How to be part of this
           <br />
         </Typography>
@@ -18,30 +28,41 @@ const HowSection = () => {
           className="text-4xl sm:text-6xl text-[#008CFF]"
           weight="700"
         >
-          &nbsp;moviment
+          &nbsp;movement
         </Typography>
         <Typography className="text-4xl sm:text-6xl text-black" weight="700">
           ?
         </Typography>
       </div>
       <CenteredElement className="w-4/5 sm:w-[65%] md:w-[50%]">
-        <CenteredElement className="py-2 px-4 sm:px-8 tablet:px-2 bg-[linear-gradient(96.71deg,_#5656F2_0%,_#3333DC_101.64%)] [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] hover:cursor-pointer">
+        <CenteredElement
+          onClick={handleToggle}
+          className={twMerge(
+            "py-2 px-4 sm:px-8 tablet:px-2 [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] hover:cursor-pointer",
+            "bg-[linear-gradient(96.71deg,_#5656F2_0%,_#3333DC_101.64%)]"
+          )}
+        >
           <Typography className="text-sm sm:text-base text-white" weight="700">
-            {/* Para Doadores */}
             For Sponsors
           </Typography>
         </CenteredElement>
-        <CenteredElement className="py-2 px-4 sm:px-8 tablet:px-2 bg-[linear-gradient(96.71deg,_rgba(86,_86,_242,_0.4)_0%,_rgba(51,_51,_220,_0.4)_101.64%)] [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] -rotate-180 hover:cursor-pointer">
+        <CenteredElement
+          onClick={handleToggle}
+          className={twMerge(
+            "py-2 px-4 sm:px-8 tablet:px-2 [box-shadow:0px_5.10638px_12.766px_#E7E7FB] rounded-tl-[45.94px] rounded-br-none rounded-tr-none rounded-bl-[45.94px] -rotate-180 hover:cursor-pointer",
+            "bg-[#3939DF]/10"
+          )}
+        >
           <Typography
-            className="text-sm sm:text-base text-white -rotate-180"
+            className="text-sm sm:text-base text-[#3F5BD9] -rotate-180"
             weight="700"
           >
-            {/* Para Artistas */}
             For Artists
           </Typography>
         </CenteredElement>
       </CenteredElement>
-      <HowToJoinCard />
+      <HowToJoinCard isSponsor={isSponsor} />
+      <Button className="mt-8" label="See Details" onClick={handleClick} />
     </Container>
   );
 };
